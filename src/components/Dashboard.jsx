4fad96187321b7e17b8fc4e214744ff86a165062
Jennifer Russell <jc.russell@yahoo.com>
2020-12-01 18:44:59 -0500

    imported font awesome

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,12 +1,16 @@
 import React from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlusCircle } from '@fortawesome/free-solid-svg-icons'
+
+const element = <FontAwesomeIcon icon={faPlusCircle} />
 
 function Dashboard() {
     return(
         <div>
         
-       <h1> Notes App</h1>
+       <h1 className="header"> Notes App</h1>
+       <span className="add-note">{element}</span>
         </div>
     )
 }
-
 export default Dashboard
